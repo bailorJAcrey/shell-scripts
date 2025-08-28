8a6ab24078eed49b98db55f3ac8c8676a27cbc82
sudonym_ <bailorjacrey@gmail.com>
2025-08-28 16:51:58 -0500

    [screen-cap.sh] Add full screenshot functionality (with multi-monitor support!)

--- screen-cap.sh
+++ screen-cap.sh
@@ -10,7 +10,17 @@ snip() {
         | xargs -I {} grim -g {} "$capture_dir$file_name.png"
 }
 
+shot() {
+    [ "$(hyprctl monitors -j | jq 'length')" == "1" ] \
+        && grim "$capture_dir$file_name.png" \
+        || ( \
+            slurp -c $slurp_border_color -b $slurp_background_color -o \
+            | xargs -I {} grim -g {} "$capture_dir$file_name.png" \
+        )
+}
+
 case $1 in
     "snip") snip ;;
+    "shot") shot ;;
     *) exit 1 ;;
 esac
